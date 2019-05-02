2780af39ab077629f77ea852143dc86ee2def414
sbboard <colin.buffum@gmail.com>
2019-05-01 23:33:57 -0600

    reformated some stuff. idk i'm just waiting for credentials

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let eBay = require('ebay-node-api')
-const path = require('path'),
-    config = require(path.join(__dirname, 'config.js'));
+const path = require('path')
+const config = require(path.join(__dirname, 'config.js'));
  
 let ebay = new eBay({
     clientID: config.client_id,
@@ -10,14 +10,19 @@ let ebay = new eBay({
     }
 });
 
-function ebaySearch(endDate){
+//function ebaySearch(endDate){
     ebay.getAccessToken()
         .then((data) => {
             ebay.searchItems({
                 keyword: "gameboy color",
                 categoryId: 139973,
                 limit: 10,
-                filter: `price:[..10],priceCurrency:USD,conditionIds:{7000},itemEndDate:[..${endDate}]` 
+                filter: `
+                        price:[..10],
+                        priceCurrency:USD,
+                        conditionIds:{7000},
+                        itemEndDate:[..${endDate}]
+                        ` 
                 //Format here: https://developer.ebay.com/api-docs/buy/static/ref-buy-browse-filters.html#conditionIds
             })
             .then((data) => {
@@ -27,15 +32,15 @@ function ebaySearch(endDate){
     .catch((err) => {
         console.log(err)
     })
-}
+//}
 
-setTimeout(()=> {
+//setTimeout(()=> {
     //get time
     //if time is 7AM
     //set endDate to today
-    let searchResults = ebaySearch(endDate)
-    if(searchResults.length > 0){
+//    let searchResults = ebaySearch(endDate)
+//    if(searchResults.length > 0){
         //email myself search results
-        console.log(searchResults)
-    }
-}, 60*60*1000);
+//        console.log(searchResults)
+//    }
+//}, 60*60*1000);
