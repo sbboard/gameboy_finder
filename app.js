fc281a3abc5b521c6dd872d132711010fbfc3c39
sbboard <colin.buffum@gmail.com>
2019-05-03 15:44:11 -0600

    added email settings

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const Ebay = require('ebay-node-api')
+const nodemailer = require("nodemailer")
 const path = require('path')
 const config = require(path.join(__dirname, 'config.js'));
+const admin = {
+    interval: 60*1000,
+    dayOf: 5
+}
 
 let ebay = new Ebay({
     clientID: config.client_id
+});
+
+let transporter = nodemailer.createTransport({
+    name: 'gang-fight.com',
+    host: 'localhost',
+    port: 25,
+    tls:{
+        rejectUnauthorized: false
+    }
 });
 
 setInterval(()=> {
@@ -18,7 +32,7 @@ setInterval(()=> {
                 let shippingCost = parseFloat(gameboy.shippingInfo[0].shippingServiceCost[0].__value__)
                 let endTime = gameboy.listingInfo[0].endTime[0]
                 let diff =  Math.floor(( Date.parse(endTime) - Date.parse(today) ) / 86400000);
-                if(gameboy.condition[0].conditionId[0] == 7000 && gameboy.primaryCategory[0].categoryId[0] == 139971 && (price + shippingCost <= 15) && diff <= 1){
+                if(gameboy.condition[0].conditionId[0] == 7000 && gameboy.primaryCategory[0].categoryId[0] == 139971 && (price + shippingCost <= 15) && diff <= admin.dayOf){
                     theBoys.push(
                         {
                             name: gameboy.title[0],
@@ -30,14 +44,23 @@ setInterval(()=> {
         })
         .then(()=>{
             if(theBoys.length > 0){
-                console.log("more")
+                let messageText = ""
+                let messageHTML = ""
+                for(let i=0;i<theBoys.length;i++){
+                    messageText += `${theBoys[i].name} ($${theBoys[i].cost}): ${theBoys[i].link}`
+                    messageHTML += `<b>${theBoys[i].name} ($${theBoys[i].cost}):</b> <a href="${theBoys[i].link}">${theBoys[i].link}</a></br>`
+                }
+                transporter.sendMail({
+                    from: '"Game Boy" <gameboy@gang-fight.com>', // sender address
+                    to: "colin.buffum@gmail.com", // list of receivers
+                    subject: "Some Good Gameboys Ending Today!", // Subject line
+                    text: messageText, // plain text body
+                    html: messageHTML // html body
+              });
             }
             else{
                 console.log("none")
             }
         })
     //}
-}, 
-    5*1000
-    //60*60*1000
-);
+}, admin.interval);
